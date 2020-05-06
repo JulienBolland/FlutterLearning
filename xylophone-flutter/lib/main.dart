c91f6a53ef98a93b4d0badb2df2c9ba16a61a206
JulienBolland <julienbolland@hotmail.com>
2020-05-06 14:03:59 +0200

    xylophone app fianl

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,10 +8,56 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        appBar: AppBar(
+          title: Text("Xylophone"),
+          backgroundColor: Colors.black,
+          centerTitle: true,
         ),
+        body: SafeArea(
+          child: Center(
+              child: MyXylophone()
+          ),
+        ),
+        backgroundColor: Colors.black,
       ),
+    );
+  }
+}
+
+class MyXylophone extends StatelessWidget {
+
+  void playSound(int nb){
+    final player = AudioCache();
+    player.play('note$nb.wav');
+  }
+
+  Expanded buildTap(Color thisColor, int nb){
+    return Expanded(
+      child: FlatButton(
+        onPressed: (){
+          playSound(nb);
+        },
+        child: Text(
+        ""
+        ),
+        color: thisColor,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        buildTap(Colors.red, 1),
+        buildTap(Colors.orange, 2),
+        buildTap(Colors.yellow, 3),
+        buildTap(Colors.green, 4),
+        buildTap(Colors.teal, 5),
+        buildTap(Colors.blue, 6),
+        buildTap(Colors.purple, 7),
+      ],
     );
   }
 }
